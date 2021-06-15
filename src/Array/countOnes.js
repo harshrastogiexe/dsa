b70cfb82f63eb05425912e74ae9b2e2ce200cad1
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-06-15 12:36:24 +0530

    First Occurence And Last Occurenve

---
+++ src/Array/countOnes.js
@@ -0,0 +1,5 @@
+const count1 = (array = []) => {
+  return firstOccurence(array, 0);
+};
+
+count1([0, 0, 0, 0, 1]);
--- src/Array/lastOccurence.js
+++ src/Array/lastOccurence.js
@@ -1,13 +1,13 @@
 // import { search } from '../Algorithm/binarySearch';
 const mid = (...val) => Math.floor(val.reduce((sum = 0, curr) => sum + curr) / val.length);
 
-export const firstOccurence = (array, key, start = 0, end = array.length - 1) => {
+export const lastOccurence = (array, key, start = 0, end = array.length - 1) => {
   while (start <= end) {
     const middle = mid(start, end);
     const middleValue = array[middle];
 
     if (middleValue === key) {
-      if (array[middle - 1] === key) end = middle;
+      if (array[middle + 1] === key) start = middle + 1;
       else return middle;
     } else if (key < middleValue) end = middle - 1;
     else start = middle + 1;
@@ -15,4 +15,4 @@ export const firstOccurence = (array, key, start = 0, end = array.length - 1) =>
   return -1;
 };
 
-console.log(firstOccurence([5, 10, 10, 10, 20, 30], 5));
+console.log(lastOccurence([5, 10, 10, 10, 20, 30], 10));
