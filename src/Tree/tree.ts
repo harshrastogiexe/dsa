504dd484a4a6b9fa1f533c20cefcec4e09990226
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-07-02 17:51:06 +0530

    Build Tree From InOrder and PreOrder

--- src/Tree/tree.ts
+++ src/Tree/tree.ts
@@ -25,27 +25,88 @@ class Tree<T> {
           return;
         }
         case 'PREORDER': {
+          callback(node.data);
           node.left && _traverse(node.left);
           node.right && _traverse(node.right);
-          callback(node.data);
           return;
         }
         case 'POSTORDER': {
-          callback(node.data);
           node.left && _traverse(node.left);
           node.right && _traverse(node.right);
+          callback(node.data);
           return;
         }
       }
     };
     _traverse(this.root);
   }
+
+  add(data: T) {
+    const node = new TreeNode(data);
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+
+    let current = this.root;
+    while (current) {
+      if (data <= current.data) {
+        if (!current.left) {
+          current.left = node;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = node;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  static buildTree(preOrder: string, inOrder: string) {
+    const tree = new Tree<string>();
+    let preIndex = 0;
+
+    const build = (inLeftIndex = 0, inRightIndex = inOrder.length - 1) => {
+      if (inLeftIndex > inRightIndex) return null;
+      const root = new TreeNode(preOrder[preIndex]);
+      preIndex++;
+
+      if (inRightIndex === inLeftIndex) return root;
+
+      let i = inLeftIndex;
+      for (; i <= inRightIndex; i++) if (root.data === inOrder[i]) break;
+
+      root.left = build(inLeftIndex, i - 1);
+      root.right = build(i + 1, inRightIndex);
+
+      return root;
+    };
+
+    tree.root = build();
+    return tree;
+  }
 }
 
+function maxNode(root: TreeNode<number>): number | undefined {
+  if (!root) return;
+
+  const leftTreeMax = root.left ? maxNode(root.left)! : root.data;
+  const rightTreeMax = root.right ? maxNode(root.right)! : root.data;
+
+  return Math.max(root.data, leftTreeMax, rightTreeMax);
+}
 const tre = new Tree<number>();
 
-tre.root = new TreeNode(2);
-tre.root.left = new TreeNode(1);
-tre.root.right = new TreeNode(3);
-
-tre.traverse(console.log, 'PREORDER');
+tre.add(2);
+tre.add(1);
+tre.add(3);
+tre.add(4);
+tre.add(0);
+tre.add(2);
+tre.add(3);
+// tre.traverse(console.log);
+Tree.buildTree('12435', '42153').traverse(console.log);
